e20a0419840899ea601529def1d0dfdf7c0adc56
rica213 <25f5k252l@mozmail.com>
2023-03-07 00:32:25 +0300

    Setup cart

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
 
 const initialState = {
-  cartItems: [],
+  cartItems: cartItems,
   amount: 0,
   total: 0,
   isLoading: true,
@@ -11,5 +12,4 @@ const cartSlice = createSlice({
   initialState,
 });
 
-//console.log(cartSlice);
 export default cartSlice.reducer;
